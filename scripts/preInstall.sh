79d528eac05ad7e6578794f0f772f68cb0369c68
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-11-14 11:33:19 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,38 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+mkdir -p ./storage/uploads
+chown -R 1000:1000 ./storage/uploads
+
+cat /opt/elestio/startPostfix.sh > post.txt
+filename="./post.txt"
+
+SMTP_LOGIN=""
+SMTP_PASSWORD=""
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Extract the values after the flags (-e)
+  values=$(echo "$line" | grep -o '\-e [^ ]*' | sed 's/-e //')
+
+  # Loop through each value and store in respective variables
+  while IFS= read -r value; do
+    if [[ $value == RELAYHOST_USERNAME=* ]]; then
+      SMTP_LOGIN=${value#*=}
+    elif [[ $value == RELAYHOST_PASSWORD=* ]]; then
+      SMTP_PASSWORD=${value#*=}
+    fi
+  done <<< "$values"
+
+done < "$filename"
+
+rm post.txt
+
+cat << EOT >> ./.env
+
+HUMHUB_MAILER_HOSTNAME=tuesday.mxrouting.net
+HUMHUB_MAILER_PORT=465
+HUMHUB_MAILER_USERNAME=${SMTP_LOGIN}
+HUMHUB_MAILER_PASSWORD=${SMTP_PASSWORD}
+HUMHUB_MAILER_SYSTEM_EMAIL_ADDRESS=${SMTP_LOGIN}
+EOT
