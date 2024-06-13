483d0fc05302554cf65ce4fcc399b384920f79d0
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-13 14:59:27 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,12 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 60s;
+
+
+sed -i "s~DOMAIN_TO_CHANGE~${DOMAIN}~g" ./docker-compose.yml
+sed -i "s~172.17.0.1~${IP}~g" ./docker-compose.yml
+
+docker-compose down;
+docker-compose up -d;
+
+sleep 30s;
